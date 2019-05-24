2ec7890d1c8ddf353030ad7cbd289d5eb8c2bb17
Anna <43002991+davailamu@users.noreply.github.com>
2019-05-24 13:48:58 +0300

    Add files via upload

---
+++ Курсовой по КЯР/Сайт(Белавская А.В.)/trips.js
@@ -0,0 +1,60 @@
+function al1() {
+            al1 = document.all.album1.style
+            back = document.all.back.style
+            al1.opacity = 1;
+            al1.zIndex = 100;
+            back.zIndex = 99;
+            back.opacity = 0.7;
+            document.all.back.onclick = function fun() {
+                al1.opacity = 0;
+                al1.zIndex = -100;
+                back.zIndex = -100;
+                back.opacity = 0;
+                window.location.reload();
+            };
+        }
+        function al2() {
+            al2 = document.all.album2.style
+            back = document.all.back.style
+            al2.opacity = 1;
+            al2.zIndex = 100;
+            back.zIndex = 99;
+            back.opacity = 0.7;
+            document.all.back.onclick = function fun() {
+                al2.opacity = 0;
+                al2.zIndex = -100;
+                back.zIndex = -100;
+                back.opacity = 0;
+                window.location.reload();
+            };
+        }
+        function al3() {
+            al3 = document.all.album3.style
+            back = document.all.back.style
+            al3.opacity = 1;
+            al3.zIndex = 100;
+            back.zIndex = 99;
+            back.opacity = 0.7;
+            document.all.back.onclick = function fun() {
+                al3.opacity = 0;
+                al3.zIndex = -100;
+                back.zIndex = -100;
+                back.opacity = 0;
+                window.location.reload();
+            };
+        }
+        function al4() {
+            al4 = document.all.album4.style
+            back = document.all.back.style
+            al4.opacity = 1;
+            al4.zIndex = 100;
+            back.zIndex = 99;
+            back.opacity = 0.7;
+            document.all.back.onclick = function fun() {
+                al4.opacity = 0;
+                al4.zIndex = -100;
+                back.zIndex = -100;
+                back.opacity = 0;
+                window.location.reload();
+            };
+        }
